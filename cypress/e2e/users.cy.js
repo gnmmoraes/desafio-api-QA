788874d5cb2d10631e210c64318e20425140c197
Gabriel Moraes <gnm_moraes@hotmail.com>
2025-06-20 00:16:42 -0300

    feat:users implementation

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -10,15 +10,9 @@ describe('Users', () => {
     administrador: 'false'
   }        
 
-  it('Cadastrar novo usuário', () => {
+  it.only('Cadastrar novo usuário', () => {
     
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: users,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+    cy.Cadastro_Usuario(users.nome, users.email, users.password, users.administrador).then((response) => {
       switch (response.status) {
         case 201:
           expect(response.body.message).to.eql('Cadastro realizado com sucesso')
@@ -33,159 +27,119 @@ describe('Users', () => {
     })
   }),
 
-  it('Cadastrar com nome em branco', () => {
+  it.only('Cadastrar com nome em branco', () => {
     let user = {
       nome: '',
       email: faker.internet.email().toLocaleLowerCase(),
       password: faker.internet.password(),
       administrador: 'false'
-    }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+    }  
+    
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.nome).to.eql('nome não pode ficar em branco')
     })
   }),
 
-  it('Payload sem atributo nome', () => {
+  it.only('Payload sem atributo nome', () => {
     let user = {
       
       email: faker.internet.email().toLocaleLowerCase(),
       password: faker.internet.password(),
       administrador: 'false'
-    }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+    }     
+    
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.nome).to.eql('nome é obrigatório')
     })
   }),
 
-  it('Cadastrar com email em branco', () => {
+  it.only('Cadastrar com email em branco', () => {
     let user = {
       nome: faker.person.fullName(),
       email: '',
       password: faker.internet.password(),
       administrador: 'false'
-    }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+    }    
+    
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.email).to.eql('email não pode ficar em branco')
     })
   }),
 
-  it('Payload sem atributo email', () => {
+  it.only('Payload sem atributo email', () => {
     let user = {
       nome: faker.person.fullName(),
       
       password: faker.internet.password(),
       administrador: 'false'
-    }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+    }     
+    
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.email).to.eql('email é obrigatório')
     })
   }),
 
-  it('Cadastrar com password em branco', () => {
+  it.only('Cadastrar com password em branco', () => {
     let user = {
       nome: faker.person.fullName(),
       email: faker.internet.email().toLocaleLowerCase(),
       password: '',
       administrador: 'false'
     }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.password).to.eql('password não pode ficar em branco')
     })
   }),
 
-  it('Payload sem atributo password', () => {
+  it.only('Payload sem atributo password', () => {
     let user = {
      nome: faker.person.fullName(),
       email: faker.internet.email().toLocaleLowerCase(),
       
       administrador: 'false'
-    }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+    }      
+    
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.password).to.eql('password é obrigatório')
     })
   }),
   
-  it('Cadastrar com administrador em branco', () => {
+  it.only('Cadastrar com administrador em branco', () => {
     let user = {
       nome: faker.person.fullName(),
       email: faker.internet.email().toLocaleLowerCase(),
       password: faker.internet.password(),
       administrador: ''
     }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.administrador).to.eql("administrador deve ser 'true' ou 'false'")
     })
   }),
 
-  it('Payload sem atributo administrador', () => {
+  it.only('Payload sem atributo administrador', () => {
     let user = {
       nome: faker.person.fullName(),
       email: faker.internet.email().toLocaleLowerCase(),
       password: faker.internet.password()
       
-    }        
-    cy.request({
-      url: '/usuarios',
-      method: 'POST',
-      body: user,
-      contentType: 'application/json',
-      failOnStatusCode: false
-    }).then((response) => {
+    }     
+    
+    cy.Cadastro_Usuario(user.nome, user.email, user.password, user.administrador).then((response) => {
       expect(response.status).to.eql(400)
       expect(response.body.administrador).to.eql('administrador é obrigatório')
     })
   }),
   
-  it('Buscar usuário por id', (id = userId) => {
+  it.only('Buscar usuário por id', (id = userId) => {
                
     cy.request({
       url: `/usuarios/${id}`,
@@ -201,7 +155,7 @@ describe('Users', () => {
     })
   }),
 
-  it('Deletar usuário por id', (id = userId) => {               
+  it.only('Deletar usuário por id', (id = userId) => {               
     cy.request({
       url: `/usuarios/${id}`,
       method: 'DELETE',      
